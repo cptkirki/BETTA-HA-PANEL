008a6be692425495cc625a17d1ea2aed7fb6442e
Cpt_Kirk <121898802+cptkirki@users.noreply.github.com>
2026-02-14 18:14:50 +0100

    Delete main directory

--- main/layout/layout_store.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "esp_err.h"
-
-esp_err_t layout_store_init(void);
-esp_err_t layout_store_load(char **json_out);
-esp_err_t layout_store_save(const char *json);
-const char *layout_store_default_json(void);
